187f28d1de80065d0dedda492a6949e0a0954c86
ramonfcf <ramonfcf@gmail.com>
2024-03-10 06:08:22 -0300

    🗑️ cleaning css and add initial structure

---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button type="button" className="btn btn-primary">
+      Left
+    </button>
+  );
+}
---
+++ src/components/pages/home.jsx
@@ -0,0 +1,10 @@
+import Button from "../button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button />
+    </div>
+  );
+}
